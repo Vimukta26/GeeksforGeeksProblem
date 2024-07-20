732accdc62930c56e10ae35a920fcc7eb2949d86
Vimukta Dashora <95113896+Vimukta26@users.noreply.github.com>
2024-07-20 21:25:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Half Nodes/remove-half-nodes.java
@@ -0,0 +1,154 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            Node fresh = g.RemoveHalfNodes(root);
+            printInorder(fresh);
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+//Back-end complete function Template for Java
+
+/*
+
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+
+*/
+
+class Solution {
+
+    // Removes all nodes with only one child and returns
+    // new root (note that root may change)
+    public Node RemoveHalfNodes(Node node) {
+        if (node == null) return null;
+
+        node.left = RemoveHalfNodes(node.left);
+        node.right = RemoveHalfNodes(node.right);
+
+        if (node.left == null && node.right == null) return node;
+
+        /* if current nodes is a half node with left
+         child NULL left, then it's right child is
+         returned and replaces it in the given tree */
+        if (node.left == null) {
+            Node new_root = node.right;
+            return new_root;
+        }
+
+        /* if current nodes is a half node with right
+           child NULL right, then it's right child is
+           returned and replaces it in the given tree  */
+        if (node.right == null) {
+            Node new_root = node.left;
+            return new_root;
+        }
+
+        return node;
+    }
+}
