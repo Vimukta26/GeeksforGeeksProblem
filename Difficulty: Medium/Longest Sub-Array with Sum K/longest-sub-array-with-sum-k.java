1590034dd76ed11ae05f7690cd282452cfef749f
Vimukta Dashora <95113896+Vimukta26@users.noreply.github.com>
2024-07-25 15:37:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
+++ Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
@@ -51,23 +51,25 @@ class Solution{
    
     // Function for finding maximum and value pair
     public static int lenOfLongSubarr (int A[], int N, int K) {
-        HashMap<Integer,Integer> map= new HashMap<>();
-        int sum=0,maxLen=0;
-        for(int i=0;i<N;i++){
-            sum+=A[i];
-            if(sum==K){
-                maxLen=i+1;
-            }
-            if(!map.containsKey(sum)){
-                map.put(sum,i);
-            }
-            if(map.containsKey(sum-K)){
-                if(maxLen<(i - map.get(sum-K))){
-                    maxLen=i-map.get(sum-K);
-                }
-            }
-        }
-        return maxLen;
+       HashMap<Integer,Integer> map=new HashMap<>();
+       int sum=0,maxLen=0;
+       for(int i=0;i<N;i++){
+           sum+=A[i];
+           if(sum==K){
+               maxLen=i+1;
+               
+           }
+           if(!map.containsKey(sum)){
+               map.put(sum,i);
+               
+           }
+           if(map.containsKey(sum-K)){
+               if(maxLen<i-(map.get(sum-K))){
+                   maxLen=i-map.get(sum-K);
+               }
+           }
+       }
+       return maxLen;
         //Complete the function
     }
     
