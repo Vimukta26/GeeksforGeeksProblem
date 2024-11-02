ba008667fbe43f230b2397a655c022a2709dcdd6
Vimukta Dashora <95113896+Vimukta26@users.noreply.github.com>
2024-11-02 20:59:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth distance/kth-distance.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public boolean checkDuplicatesWithinK(int[] arr, int k) {
+        int n = arr.length;
+        // Creates an empty HashSet
+        HashSet<Integer> myset = new HashSet<>();
+
+        // Traverse the input array
+        for (int i = 0; i < n; i++) {
+            // If already present in hashset, then we found
+            // a duplicate within k distance
+            if (myset.contains(arr[i])) {
+                return true;
+            }
+
+            // Add this item to hashset
+            myset.add(arr[i]);
+
+            // Remove the k+1 distant item
+            if (i >= k) {
+                myset.remove(arr[i - k]);
+            }
+        }
+        return false;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+            int k = Integer.parseInt(br.readLine());
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            boolean res = obj.checkDuplicatesWithinK(arr, k);
+            if (res)
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
